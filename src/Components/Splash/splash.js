56b7e8882d8244d377feccb5cc6e1339059c2261
Rehlaender <mario.rehlaender@accenture.com>
2019-02-22 12:50:52 -0600

    splash start

--- src/Components/Splash/splash.js
+++ src/Components/Splash/splash.js
@@ -1,12 +1,38 @@
 import React from 'react'
+import { getCookie } from '../localStorageHandler';
 import { Link } from 'react-router-dom';
+import { Planet } from 'react-kawaii';
+
 import './splash.css';
 
 export const Splash = props => {
+    const userCookie = getCookie();
+    console.log(userCookie, 'user cookie');
     return (
         <div className={"splash"}>
-            <h1>Splash</h1>
+            <h4>who cares?</h4>
+            <Animation />
+            <h2>we do</h2>
             <Link to="/registration">Start quizz</Link>
+        </div>
+    )
+}
+
+const Animation = props => {
+    return (
+        <div className="animationContainer">
+            <div className={"mainPlanet"}>
+                <Planet size={200} mood="sad" color="#FDA7DC" />
+            </div>
+            <div className={"bounginPlanet"}>
+                <Planet size={200} mood="happy" color="#FDA7DC" />
+            </div>
+            <div className={"spinningPlanet"}>
+                <Planet size={200} mood="blissful" color="#FDA7DC" />
+            </div>
+            <div className={"spinningPlanet"}>
+                <Planet size={200} mood="happy" color="#FDA7DC" />
+            </div>
         </div>
     )
 }
